145ab897a1f5981b6e51505c9116d5811379054d
wolyslager <olyslager.willy@gmail.com>
2020-12-01 19:54:12 -0600

    added another endpoint to handle username search

--- server.js
+++ server.js
@@ -7,12 +7,10 @@ app.use(cors());
 
 let tweets;
 
-app.get('/', (req, res) => {
-	//this is where I want to receive the user that the front end wants
-	//to search for, and return json data regarding the tweets
-	// res.send({"this": "is working"})
+app.get('/keywords', (req, res) => {
+
 	console.log(req.headers.search_value)
-	url = 'https://api.twitter.com/1.1/search/tweets.json?q='+req.headers.search_value+'&result_type=popular'
+	url = 'https://api.twitter.com/1.1/search/tweets.json?q='+req.headers.search_value+'&result_type=popular&tweet_mode=extended '
 	fetch(url, {
 		headers: {
 			'Content-Type' : 'application/json',
@@ -22,6 +20,23 @@ app.get('/', (req, res) => {
 	}).then(response => response.json())
 	  .then((data) =>{
 	  		res.send(data.statuses)
+	  })	
+})
+
+app.get('/users', (req, res) => {
+
+	console.log(req.headers.search_value)
+	url = 'https://api.twitter.com/2/tweets/search/recent?query=from:'+req.headers.search_value+'&tweet.fields=public_metrics&media.fields=public_metrics'
+	fetch(url, {
+		headers: {
+			'Content-Type' : 'application/json',
+			'Accept' : 'application/json',
+			'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAALDmJwEAAAAAix80Mtkn6ulfEVbiljn5FGk17v4%3DeHJl3DMsy09qTW2IjCFDwz5Zu5Uja5vVSDEzWJAFluT32Dtiue'
+		}
+	}).then(response => response.json())
+	  .then((data) =>{
+	  	console.log(data.data)
+	  		res.send(data.data)
 	  })	
 })
 
