8a7303a3b0ad0459534c8a6d2107712497b894de
wolyslager <olyslager.willy@gmail.com>
2020-12-06 12:01:50 -0600

    finalized routes

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const app = express();
 
 app.use(cors());
 
-let tweets;
-
-app.get('/keywords', (req, res) => {
-
-	console.log(req.headers.search_value)
+app.get('/search', (req, res) => {
 	url = 'https://api.twitter.com/1.1/search/tweets.json?q='+req.headers.search_value+'&result_type=popular&tweet_mode=extended '
 	fetch(url, {
 		headers: {
@@ -23,10 +19,8 @@ app.get('/keywords', (req, res) => {
 	  })	
 })
 
-app.get('/users', (req, res) => {
-
-	console.log(req.headers.search_value)
-	url = 'https://api.twitter.com/2/tweets/search/recent?query=from:'+req.headers.search_value+'&tweet.fields=public_metrics&media.fields=public_metrics'
+app.get('/random', (req, res) => {
+	url = 'https://api.twitter.com/1.1/search/tweets.json?q='+req.headers.search_value+'&result_type=popular&tweet_mode=extended&count=1'
 	fetch(url, {
 		headers: {
 			'Content-Type' : 'application/json',
@@ -35,10 +29,10 @@ app.get('/users', (req, res) => {
 		}
 	}).then(response => response.json())
 	  .then((data) =>{
-	  	console.log(data.data)
-	  		res.send(data.data)
+	  		res.send(data.statuses)
 	  })	
 })
+
 
 app.listen(3000, ()=> {
 	console.log('app is running on port 3000')
