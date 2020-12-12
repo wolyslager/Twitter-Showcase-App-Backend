ac6ece0300024574884928f8f5a06ba0a7814ce0
wolyslager <olyslager.willy@gmail.com>
2020-12-11 19:56:02 -0600

    added route for random profiles

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fetch = require('node-fetch');
 const app = express();
 
 app.use(cors());
+
 app.get('/search', (req, res) => {
 	url = 'https://api.twitter.com/1.1/search/tweets.json?q='+req.headers.search_value+'&result_type=popular&tweet_mode=extended '
 	fetch(url, {
@@ -19,6 +20,38 @@ app.get('/search', (req, res) => {
 })
 
 app.get('/search-user', (req, res) => {
+	console.log('HELLO')
+	let dataArray = []
+	url = 'https://api.twitter.com/1.1/users/show.json?screen_name=' + req.headers.search_value
+	fetch(url, {
+		headers: {
+			'Content-Type' : 'application/json',
+			'Accept' : 'application/json',
+			'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAALDmJwEAAAAAix80Mtkn6ulfEVbiljn5FGk17v4%3DeHJl3DMsy09qTW2IjCFDwz5Zu5Uja5vVSDEzWJAFluT32Dtiue'
+		}
+	}).then(response => response.json())
+	  .then((data) =>{
+	  		console.log(data)
+	  		dataArray.push(data)
+	  		url = 'https://api.twitter.com/1.1/search/tweets.json?q=from:' + req.headers.search_value + '&result_type=popular&tweet_mode=extended'
+			fetch(url, {
+				headers: {
+					'Content-Type' : 'application/json',
+					'Accept' : 'application/json',
+					'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAALDmJwEAAAAAix80Mtkn6ulfEVbiljn5FGk17v4%3DeHJl3DMsy09qTW2IjCFDwz5Zu5Uja5vVSDEzWJAFluT32Dtiue'
+				}
+			}).then(response => response.json())
+			  .then((data) =>{
+			  		console.log(data)
+			  		dataArray.push(data)
+			  		res.send(dataArray)
+			  })
+	  })
+	
+})
+
+app.get('/random-users', (req, res) => {
+	console.log('HELOOOOOOOOO')
 	url = 'https://api.twitter.com/1.1/users/show.json?screen_name=' + req.headers.search_value
 	fetch(url, {
 		headers: {
@@ -30,8 +63,11 @@ app.get('/search-user', (req, res) => {
 	  .then((data) =>{
 	  		console.log(data)
 	  		res.send(data)
-	  })	
+	  })
+	
 })
+
+
 
 app.get('/random', (req, res) => {
 	url = 'https://api.twitter.com/1.1/search/tweets.json?q='+req.headers.search_value+'&result_type=popular&tweet_mode=extended&count=1'
